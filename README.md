6cb70cad4546b276e55ca9d9eaa59f1c20065f09
oem <darkoem@gmail.com>
2010-12-07 21:26:23 +0100

    Add fade2

---
+++ README.md
@@ -0,0 +1,10 @@
+# pre2k asm stuff
+
+This is some of the very old assembler code that I was able to salvage, mostly because I had hardcopies of the sourcecode.
+
+This is all coded under DOS and mostly with TASM.
+
+## Deconstruction
+
+If you spot an error please say so, the prints where not in the best of states and I had to fill in a few lines.  
+My assembler is slightly rusty these days, especially regarding those highly optimized graphic operations.
---
+++ fade2.asm
@@ -0,0 +1,33 @@
+; Fade from one image to another
+
+Fade2 proc near
+; DS:SI^2 = target dactable, ES:DI^2 dest. dactable
+  pusha
+  mov cx, 768
+  push di
+  push si
+fade_loop:
+  mov al, [si]
+  mov ah, [di]
+  cmp al, ah
+  je fade_loop2
+  ja fade_loop1
+  inc al
+  jmp fade_loop2
+fade_loop1:
+  dec al
+  mov [si], al
+  inc si
+  inc di
+  loop fade_loop
+fade_loop2:
+  pop di
+  pop si
+  call VWAIT
+  call SETPAL
+  popa
+  ret
+Fade2 endp
+
+; This fades only one step, to totally fade you need to call this 63 times.
+; This is so you can combine it with other effects
